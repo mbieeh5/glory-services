733ac9fbea91dfa032b0c7441fe375f7cf3d19fb
mbieeh5 <mbieeh5@gmail.com>
2024-10-18 20:10:03 +0700

    update Styling from Table & etc

--- components/Table.tsx
+++ components/Table.tsx
@@ -69,35 +69,35 @@ const Columns: TableProps<DataRes>["columns"] = [
     width: 160,  
     align: "center",
     render: (text) => text.toLocaleUpperCase(),
-  },
-  {
-    title: "KERUSAKAN",
-    dataIndex: "Kerusakan",
-    key: "Kerusakan",
-    width: 170,
-    align: "center",
-    render: (text) => text.toLocaleUpperCase(),
-},
-  {
-      title: "SPAREPARTS",
+    },
+    {
+        title: "KERUSAKAN",
+        dataIndex: "Kerusakan",
+        key: "Kerusakan",
+        width: 200,
+        align: "center",
+        render: (text) => text.toLocaleUpperCase(),
+    },
+    {
+    title: "SPAREPARTS",
     dataIndex: "sparepart",
     key: "sparepart",
-    width: 210,
+    width: 700,
     align: "center",
     render: (_, { sparepart }) => (
-        <>
+        <WrapperExpandable2>
         {sparepart?.map((item, index) => {
             const color = getColor(index);
           return (
-            <>
-            <Tag key={`${item.Sparepart}-${index}`} color={color}>
-              {item.Sparepart.toLocaleUpperCase()}({item.TypeOrColor})
-            </Tag>
-              <Tag color={color}>Rp {parseInt(item.HargaSparepart).toLocaleString()}</Tag>
-            </>
+            <div key={`${item.Sparepart}-${index}`} style={{alignItems: 'center', textAlign: 'center', width: '100%'}}>
+                <Tag color={color}>
+                {item.Sparepart.toLocaleUpperCase()}({item.TypeOrColor})
+                    Rp {parseInt(item.HargaSparepart).toLocaleString()}
+                </Tag>
+            </div>
           );
         })}
-      </>
+      </WrapperExpandable2>
     ),
   },
   {
@@ -108,65 +108,75 @@ const Columns: TableProps<DataRes>["columns"] = [
     align: 'center',
     render: (value) => `Rp${parseInt(value).toLocaleString()}`,
   },
+
   {
       title: "IMEI",
       dataIndex: "Imei",
       key: "Imei",
       align: 'center',
       ellipsis: true,
+      hidden: true,
     },
     {
         title: "NAMA USER",
         dataIndex: "NamaUser",
-    key: "NamaUser",
-    ellipsis: true,
-},
-{
-      title: "NO.USER",
-      dataIndex: "NoHpUser",
-      key: "NoHpUser",
-      ellipsis: true,
-},
-{
-    title: "TEKNISI",
-    dataIndex: "Teknisi",
-    key: "Teknisi",
-    ellipsis: true,
-},
-{
-    title: "HARGA IBNU",
-    dataIndex: 'HargaIbnu',
-    key: "HargaIbnu",
-    ellipsis: true,
-    render: (text) => text ? `Rp ${parseInt(text).toLocaleString()}` : 'Rp 0',
-},
-{
-    title: "PENERIMA",
-    dataIndex: "Penerima",
-    key: "Penerima",
-    ellipsis: true,
-},
-{
-    title: "NO NOTA",
-    dataIndex: "NoNota",
-    key: "NoNota",
-    align: "center",
-    ellipsis: true,
-},
-{
-    title: "TANGGAL KELUAR",
-    dataIndex: "TglKeluar",
-    key: "TglKeluar",
-    align: "center",
-    ellipsis: true,
-    render: (text) => text ? (text === 'null' ? 'BELUM DIAMBIL' : dateFormater(text)) : 'BELUM DIAMBIL'
-},
-{
-    title: "STATUS",
-    dataIndex: "status",
-    key: "status",
-    ellipsis: true, 
-    render: (status) => (
+        key: "NamaUser",
+        ellipsis: true,
+        hidden: true,
+    },
+    {
+        title: "NO.USER",
+        dataIndex: "NoHpUser",
+        key: "NoHpUser",
+        ellipsis: true,
+        hidden: true,
+    },
+    {
+        title: "TEKNISI",
+        dataIndex: "Teknisi",
+        key: "Teknisi",
+        ellipsis: true,
+        hidden: true,
+    },
+    {
+        title: "HARGA IBNU",
+        dataIndex: 'HargaIbnu',
+        key: "HargaIbnu",
+        ellipsis: true,
+        hidden: true,
+        render: (text) => text ? `Rp ${parseInt(text).toLocaleString()}` : 'Rp 0',
+    },
+    {
+        title: "PENERIMA",
+        dataIndex: "Penerima",
+        key: "Penerima",
+        ellipsis: true,
+        hidden: true,
+    },
+    {
+        title: "NO NOTA",
+        dataIndex: "NoNota",
+        key: "NoNota",
+        align: "center",
+        ellipsis: true,
+        hidden: true,
+    },
+    {
+        title: "TANGGAL KELUAR",
+        dataIndex: "TglKeluar",
+        key: "TglKeluar",
+        align: "center",
+        hidden: true,
+        ellipsis: true,
+        render: (text) => text ? (text === 'null' ? 'BELUM DIAMBIL' : dateFormater(text)) : 'BELUM DIAMBIL'
+    },
+    {
+        title: "STATUS",
+        dataIndex: "status",
+        key: "status",
+        ellipsis: true, 
+        hidden: true,
+        render: (status) => (
         <Tag color={status === "sukses" ? "green" : status === "process" ? 'yellow' : status === 'cancel' ? "red" : 'warning'}>{status}</Tag>
     ),
   },
@@ -182,14 +192,25 @@ const Table: React.FC<TableComponentProps> = ({ data }) => {
             <AntdTable<DataRes> 
             columns={Columns} 
             dataSource={data} 
-            rowKey="NoNota" 
+            rowKey="NoNota"
             size={"small"}
-            scroll={{ x: 'calc(1100px + 50%', y: 130 * 5 }}
+            pagination={false}
+            scroll={{ x: 0 , y: 130 * 5 }}
+            expandable={{
+                expandedRowRender: (record) => 
+                    <WrapperExpandable status={record?.status || ""} tglKeluar={record?.TglKeluar || null}>
+                        <Tag style={{display: 'flex'}} color={'default'}>NO NOTA <div style={{marginLeft: '49px'}}>:</div> <div style={{marginLeft: '1%'}}> {record.NoNota} </div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>IMEI <div style={{marginLeft: '80px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.Imei.length > 5 ? record.Imei : "IMEI KOSONG"}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>NAMA USER <div style={{marginLeft: '34px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.NamaUser?.toLocaleUpperCase()}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>NO HP USER <div style={{marginLeft: '34px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.NoHpUser}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>TEKNISI <div style={{marginLeft: '61px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.Teknisi ? record.Teknisi.toLocaleUpperCase() : 'BELUM DIKERJAKAN'}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>HARGA IBNU <div style={{marginLeft: '32px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.HargaIbnu ? record.HargaIbnu : 0} </div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>PENERIMA <div style={{marginLeft: '47px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.Penerima?.toLocaleUpperCase()}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={'default'}>TANGGAL KELUAR <div style={{marginLeft: '5px'}}>:</div> <div style={{marginLeft: '1%'}}>{record.TglKeluar ? dateFormater(record.TglKeluar) : "BELUM DI AMBIL"}</div></Tag>
+                        <Tag style={{display: 'flex'}} color={record.status === 'sukses' ? 'green-inverse' : record.status === 'process' ? 'gold-inverse' : 'volcano-inverse'}>{record.status.toLocaleUpperCase()}</Tag>
+                    </WrapperExpandable>
+            }}
             components={{
-                header: 
-                {
-                    
-                },
                 body: 
                 {
                     row: (props: { children: ReactNode; "data-row-key" ?: string}) => {
@@ -208,6 +229,22 @@ const Table: React.FC<TableComponentProps> = ({ data }) => {
         )
 };
 
+const WrapperExpandable = styled.div<{status: string, tglKeluar: string | null}>`
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+  color: rgb(var(--text));
+  font-weight: bold;
+  width: 100%;
+`;
+
+const WrapperExpandable2 = styled.div`
+display: flex; 
+flex-direction: row;
+font-weight: bold;
+`;
+
+
 
 const StyledRow = styled.tr<{ status: string; tglKeluar: string | null }>`
   background-color: ${({ status, tglKeluar }) => {
@@ -215,7 +252,9 @@ const StyledRow = styled.tr<{ status: string; tglKeluar: string | null }>`
     if (status === "sukses") return "#12D200";
     if (status === "process") return "#EDE835";
     if (status === "process" && tglKeluar === 'null') return "#F57E7E";
-    return "#DB3759";
+    if (status === 'cancel') return "#DB3759";
+    if (status === 'claim garansi') return "gray";
+    return "rgb(var(--background))";
   }};
   font-size: 12px;
 `;
